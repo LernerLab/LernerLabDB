16ef066141f1993642324bdfb532afe327fac95f
mschaid <michael.schaid@northwestern.edu>
2023-10-24 11:16:43 -0500

    updated docstring

--- lernerlabdb/lernerlabdb/interface_modules/Procedure.py
+++ lernerlabdb/lernerlabdb/interface_modules/Procedure.py
@@ -1,29 +1,62 @@
 from lernerlabdb.interface_modules.Structure import Structure
+from typing import List, Dict
 
 
 class Procedure:
-    def __init__(self, name, id):
+    """
+    A class representing an isolated prodcedure during a surgery. Includes structures, injections, implants and associated coordintes.
+
+    Parameters
+    ----------
+    name : str
+        The name of the procedure.
+    id : int
+        The ID of the procedure.
+
+    Attributes
+    ----------
+    name : str
+        The name of the procedure.
+    id : int
+        The ID of the procedure.
+    structures : list
+        A list of structures associated with the procedure.
+
+    Methods
+    -------
+    add_structure(structure: Structure)
+        Adds a structure to the list of structures associated with the procedure.
+    procedure_data()
+        Returns a dictionary containing the procedure's name, ID, and associated structures.
+    """
+
+    def __init__(self, name: str, id: int):
         self._name = name
         self._id = id  # ! I think we need a way to auto-generate this and cross reference the db, but placeholder for now
-        self._structures = []
+        self._structures: List = []
 
     @property
-    def name(self):
+    def name(self) -> str:
+        '''The name of the procedure'''
         return self._name
 
     @property
-    def id(self):
+    def id(self) -> int:
+        '''The ID of the procedure'''
         return self._id
 
     @property
     def structures(self):
+        '''A list of structures associated with the procedure'''
         return self._structures
 
-    def add_structure(self, structure: Structure):
+    def add_structure(self, structure: Structure) -> None:
+        '''Adds a Structure object to the list of structures associated with the procedure'''
         self._structures.append(structure)
 
     @property
-    def procedure_data(self):
+    def procedure_data(self) -> Dict:
+        '''Returns a dictionary containing all associated data for the procedure'''
         data = {
             "name": self.name,
             "id": self.id,
