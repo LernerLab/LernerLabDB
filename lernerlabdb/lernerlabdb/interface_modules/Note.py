128871ee2a9d93a1cef67aabc6d334959fc1a9fa
mschaid <michael.schaid@northwestern.edu>
2023-10-24 14:29:24 -0500

    note class with tests passed

---
+++ lernerlabdb/lernerlabdb/interface_modules/Note.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from typing import Dict, NewType, Optional, Literal, Any
+
+
+Date = NewType("Date", datetime.date)
+Time = NewType("Time", datetime.time)
+
+
+class Note:
+    """
+    A class representing a note about a medical procedure or recovery.
+
+    Attributes:
+        type (Literal['Surgery', 'Recovery', 'Experimental', 'Other']): The type of the note.
+        note (Any): The content of the note.
+        date_ (str): The date of the note in the format "MM/DD/YYYY".
+        time_ (str): The time of the note in the format "HH:MM".
+        note_data (Dict[str, Any]): A dictionary containing the note data, including the date, time, type, and note content.
+
+    Raises:
+        ValueError: If the `type` argument is not one of the allowed options.
+
+    """
+
+    def __init__(self,
+                 type: Literal['Surgery', 'Recovery', 'Experimental', 'Other'],
+                 note):
+        self.date: Date = datetime.now().date()
+        self.time: Time = datetime.now().time()
+
+        if type not in ['Surgery', 'Recovery', 'Experimental', 'Other']:
+            raise ValueError(
+                "Note type must be one of: 'Surgery', 'Recovery', 'Experimental', 'Other'")
+        self.type = type
+        self.note = note
+
+    @property
+    def date_(self) -> str:
+        """The date of the note in the format "MM/DD/YYYY"""
+        return self.date.strftime("%m/%d/%Y")
+
+    @property
+    def time_(self) -> str:
+        """The time of the note in the format "HH:MM"."""
+        return self.time.strftime("%H:%M")
+
+    @property
+    def note_data(self) -> Dict[str, Any]:
+        """A dictionary containing the note data, including the date, time, type, and note content."""
+        data = {"date": self.date_,
+                "time": self.time_,
+                "type": self.type,
+                "note": self.note}
+        return data
---
+++ lernerlabdb/tests/interface_modules_tests/test_Note.py
@@ -0,0 +1,36 @@
+import pytest
+from datetime import datetime
+
+from lernerlabdb.interface_modules.Note import Note
+
+
+class TestNote:
+
+    def test_note_initialization(self):
+        note = Note('Surgery', 'This is a test note.')
+        assert isinstance(
+            note, Note), "Note object was not created successfully."
+
+    def test_note_type_validation(self):
+        with pytest.raises(ValueError):
+            Note('InvalidType', 'This should raise a ValueError.')
+
+    def test_note_date_property(self):
+        note = Note('Recovery', 'Testing date property.')
+        assert note.date_ == datetime.now().strftime(
+            "%m/%d/%Y"), "Date property is incorrect."
+
+    def test_note_time_property(self):
+        note = Note('Experimental', 'Testing time property.')
+        current_time = datetime.now().strftime("%H:%M")
+        assert note.time_ == current_time, "Time property is incorrect."
+
+    def test_note_data_property(self):
+        note = Note('Other', 'Testing note_data property.')
+        expected_data = {
+            "date": note.date_,
+            "time": note.time_,
+            "type": 'Other',
+            "note": 'Testing note_data property.'
+        }
+        assert note.note_data == expected_data, "note_data property is incorrect."
