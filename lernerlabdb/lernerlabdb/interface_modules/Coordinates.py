92bd1505bdbf2032cb1717414140aca76ee9f2b2
mschaid <michael.schaid@northwestern.edu>
2023-11-06 15:15:53 -0600

    update coordinate defaults: tests passed

--- lernerlabdb/lernerlabdb/interface_modules/Coordinates.py
+++ lernerlabdb/lernerlabdb/interface_modules/Coordinates.py
@@ -1,18 +1,14 @@
+from typing import Dict
 
 
 class Coordinates:
-    def __init__(self, ap: float = 0, ml: float = 0, dv: float = 0):
-        self.ap = ap
-        self.ml = ml
-        self.dv = dv
-
-    # def __repr__(self):
-    #     if self.ap is None or self.ml is None or self.dv is None:
-    #         return "Coordinates not logged. Please log coordinates."
-    #     return f"Coordinates(AP = {self.ap}, ML = {self.ml}, DV = {self.dv})"
+    def __init__(self, ap: float = 0., ml: float = 0., dv: float = 0.):
+        self.ap: float = ap
+        self.ml: float = ml
+        self.dv: float = dv
 
     @property
-    def coordinates(self) -> dict:
+    def coordinates(self) -> Dict[str, float]:
         # if self.ap is None or self.ml is None or self.dv is None:
         #     return "Coordinates not logged. Please log coordinates."
         coordinates = {"AP": self.ap, "ML": self.ml, "DV": self.dv}
--- lernerlabdb/tests/interface_modules_tests/test_Coordinates.py
+++ lernerlabdb/tests/interface_modules_tests/test_Coordinates.py
@@ -4,13 +4,13 @@ from lernerlabdb.interface_modules.Coordinates import Coordinates
 class TestCoordinates:
     def test_init(self):
         coordinates = Coordinates(ap=1.0, ml=2.0, dv=3.0)
-        none_coordinates = Coordinates()
+        zero_coordinates = Coordinates()
         assert coordinates.ap == 1.0
         assert coordinates.ml == 2.0
         assert coordinates.dv == 3.0
-        assert none_coordinates.ap is None
-        assert none_coordinates.ml is None
-        assert none_coordinates.dv is None
+        assert zero_coordinates.ap is ==0
+        assert zero_coordinates.ml is ==0
+        assert zero_coordinates.dv is ==0
 
     def test_repr(self):
         coordinates = Coordinates(ap=1.0, ml=2.0, dv=3.0)
@@ -20,11 +20,3 @@ class TestCoordinates:
         coordinates = Coordinates(ap=1.0, ml=2.0, dv=3.0)
         assert coordinates.coordinates == {"AP": 1.0, "ML": 2.0, "DV": 3.0}
 
-    def test_reper_none(self):
-        coordinates = Coordinates()
-        assert repr(
-            coordinates) == "Coordinates not logged. Please log coordinates."
-
-    def test_coordinates_none(self):
-        coordinates = Coordinates()
-        assert coordinates.coordinates == "Coordinates not logged. Please log coordinates."
