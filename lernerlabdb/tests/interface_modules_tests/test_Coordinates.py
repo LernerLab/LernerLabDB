df8f1809d256cdcdae925ffef01795e5f52f7343
mschaid <michael.schaid@northwestern.edu>
2023-11-06 17:11:48 -0600

    fixed test bugs

--- lernerlabdb/tests/interface_modules_tests/test_Coordinates.py
+++ lernerlabdb/tests/interface_modules_tests/test_Coordinates.py
@@ -8,15 +8,10 @@ class TestCoordinates:
         assert coordinates.ap == 1.0
         assert coordinates.ml == 2.0
         assert coordinates.dv == 3.0
-        assert zero_coordinates.ap is ==0
-        assert zero_coordinates.ml is ==0
-        assert zero_coordinates.dv is ==0
-
-    def test_repr(self):
-        coordinates = Coordinates(ap=1.0, ml=2.0, dv=3.0)
-        assert repr(coordinates) == "Coordinates(AP = 1.0, ML = 2.0, DV = 3.0)"
+        assert zero_coordinates.ap == 0
+        assert zero_coordinates.ml == 0
+        assert zero_coordinates.dv == 0
 
     def test_coordinates(self):
         coordinates = Coordinates(ap=1.0, ml=2.0, dv=3.0)
         assert coordinates.coordinates == {"AP": 1.0, "ML": 2.0, "DV": 3.0}
-
--- lernerlabdb/tests/interface_modules_tests/test_Drug.py
+++ lernerlabdb/tests/interface_modules_tests/test_Drug.py
@@ -7,7 +7,7 @@ from lernerlabdb.interface_modules.enums import DrugType
 class TestDrug:
     def test_valid_init(self):
         test_drug = Drug(DrugType.BUPESR, 0.3, 0.2)
-        assert is instance(self._substance, DrugType)
+        assert isinstance(test_drug._substance, DrugType)
         assert test_drug.substance == "BupeSR"
         assert test_drug.dose == 0.3
         assert test_drug.volume_administered == 0.2
@@ -17,4 +17,3 @@ class TestDrug:
                          "volume administered (mL)": 0.2}
 
         assert test_drug.data == expected_data
-
