c9786d1174cf4fd4cd54c086fd2dae2b273b03c2
mschaid <michael.schaid@northwestern.edu>
2023-11-07 16:01:43 -0600

    fixtures updated

--- lernerlabdb/tests/interface_modules_tests/test_Structure.py
+++ lernerlabdb/tests/interface_modules_tests/test_Structure.py
@@ -6,19 +6,31 @@ from lernerlabdb.interface_modules.Coordinates import Coordinates
 from lernerlabdb.interface_modules.enums import ImplantType, Hemisphere, InjectionType
 
 
-class TestStructure:
-    def test_init(self):
+@pytest.fixture
+def structure():
+    return Structure("Lateral Hypothalamic Area",
+                     "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
 
-        structure = Structure("Lateral Hypothalamic Area",
-                              "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
+
+@pytest.fixture
+def injection():
+    return Injection(substrate="AAV-dLIGHT1.3b", type=InjectionType.VIRUS,
+                     titer=1.0, volume=500, flowrate=100)
+
+
+@pytest.fixture
+def implant():
+    return Implant(type=ImplantType.OPTO)
+
+
+class TestStructure:
+    def test_init(self, structure):
         assert structure.region == "LATERAL HYPOTHALAMIC AREA"
         assert structure.accronym == "LHA"
         assert structure.hemisphere == "Left"
         assert structure.coordinates == {"AP": -1.6, "ML": 0.9, "DV": -4.9}
 
-    def test_valid_instance(self):
-        structure = Structure("Lateral Hypothalamic Area",
-                              "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
+    def test_valid_instance(self, structure):
 
         assert isinstance(structure.region, str)
         assert isinstance(structure.accronym, str)
@@ -31,58 +43,38 @@ class TestStructure:
         assert isinstance(structure._coordinates.ml, float)
         assert isinstance(structure._coordinates.dv, float)
 
-    def test_add_injection(self):
-        structure = Structure("Lateral Hypothalamic Area",
-                              "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
+    def test_add_injection(self, structure, injection):
 
-        inj = Injection(substrate="AAV-dLIGHT1.3b", type=InjectionType.VIRUS,
-                        titer=1.0, volume=500, flowrate=100)
-        structure.add_injection(inj)
+        structure.add_injection(injection)
 
         assert len(structure.injections) == 1
-        assert structure.injections[0] == inj
+        assert structure.injections[0] == injection
         assert structure.number_of_injections == 1
 
-    def test_remove_injection(self):
-        structure = Structure("Lateral Hypothalamic Area",
-                              "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
+    def test_remove_injection(self, structure, injection):
 
-        inj = Injection(substrate="AAV-dLIGHT1.3b", type=InjectionType.VIRUS,
-                        titer=1.0, volume=500, flowrate=100)
-        structure.add_injection(inj)
+        structure.add_injection(injection)
         assert len(structure.injections) == 1
-        structure.remove_injection(inj)
+        structure.remove_injection(injection)
         assert len(structure.injections) == 0
 
-    def test_add_implant(self):
-        structure = Structure("Lateral Hypothalamic Area",
-                              "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
-        imp = Implant("OPTO")
-        structure.add_implant(imp)
-
+    def test_add_implant(self, structure, implant):
+        structure.add_implant(implant)
         assert len(structure.implants) == 1
-        assert structure.implants[0] == imp
+        assert structure.implants[0] == implant
         assert structure.number_of_implants == 1
 
-    def test_remove_implant(self):
-        structure = Structure("Lateral Hypothalamic Area",
-                              "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
-        imp = Implant("OPTO")
-        structure.add_implant(imp)
+    def test_remove_implant(self, structure, implant):
+        structure.add_implant(implant)
         assert len(structure.implants) == 1
-        structure.remove_implant(imp)
+        structure.remove_implant(implant)
         structure.implants == []
         structure.number_of_implants == 0
 
-    def test_structure_data(self):
-        structure = Structure("Lateral Hypothalamic Area",
-                              "LHA", Hemisphere.LEFT, (-1.6, 0.9, -4.9))
-        inj = Injection(substrate="AAV-dLIGHT1.3b", type=InjectionType.VIRUS,
-                        titer=1.0, volume=500, flowrate=100)
+    def test_structure_data(self, structure, injection, implant):
 
-        inj.adjust_injection_coordinates(1, 2, 3)
-        structure.add_injection(inj)
-        implant = Implant(type=ImplantType.OPTO)
+        injection.adjust_injection_coordinates(1, 2, 3)
+        structure.add_injection(injection)
         implant.adjust_implant_coordinates(1, 2, 3)
         structure.add_implant(implant)
 
