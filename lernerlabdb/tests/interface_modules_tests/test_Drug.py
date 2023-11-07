93c3026e3dbecba282197eac99859415ea015e0e
mschaid <michael.schaid@northwestern.edu>
2023-11-07 15:54:12 -0600

    updated tests

--- lernerlabdb/tests/interface_modules_tests/test_Drug.py
+++ lernerlabdb/tests/interface_modules_tests/test_Drug.py
@@ -4,16 +4,21 @@ from lernerlabdb.interface_modules.Drug import Drug
 from lernerlabdb.interface_modules.enums import DrugType
 
 
+@pytest.fixture
+def drug():
+    return Drug(DrugType.BUPESR, 0.3, 0.2)
+
+
 class TestDrug:
-    def test_valid_init(self):
-        test_drug = Drug(DrugType.BUPESR, 0.3, 0.2)
-        assert isinstance(test_drug._substance, DrugType)
-        assert test_drug.substance == "BupeSR"
-        assert test_drug.dose == 0.3
-        assert test_drug.volume_administered == 0.2
+    def test_valid_init(self, drug):
+
+        assert isinstance(drug._substance, DrugType)
+        assert drug.substance == "BupeSR"
+        assert drug.dose == 0.3
+        assert drug.volume_administered == 0.2
 
         expected_data = {"substance": "BupeSR",
                          "dose (mg/kg)": 0.3,
                          "volume administered (mL)": 0.2}
 
-        assert test_drug.data == expected_data
+        assert drug.data == expected_data
