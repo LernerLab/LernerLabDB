6c6eea4724e44305a79bf840ff4762ca3a8ec135
mschaid <michael.schaid@northwestern.edu>
2023-11-09 15:46:46 -0600

    Refactor Scientist class to add multiple projects at once and add data property. tests passed

--- lernerlabdb/lernerlabdb/interface_modules/Scientist.py
+++ lernerlabdb/lernerlabdb/interface_modules/Scientist.py
@@ -1,15 +1,15 @@
-from dataclasses import dataclass
+
 from lernerlabdb.interface_modules.Project import Project
 from typing import List
 
 
-
 class Scientist:
-    first_name:str
-    last_name:str
-    email:str
-    net_id:str
-    projects:List[Project]
+    first_name: str
+    last_name: str
+    email: str
+    net_id: str
+    projects: List[Project]
+
     def __init__(self, first_name, last_name, email, net_id):
         self.first_name = first_name
         self.last_name = last_name
@@ -17,8 +17,20 @@ class Scientist:
         self.net_id = net_id
         self.projects = []
 
-    def add_project(self, project: Project):
-        self.projects.append(project)
+    def add_project(self, *projects):
+        for project in projects:
+            if isinstance(project, list):
+                self.projects.extend(project)
+            else:
+                self.projects.append(project)
 
-
-
+    @property
+    def data(self):
+        data = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'net_id': self.net_id,
+            'projects': [p.data for p in self.projects]
+        }
+        return data
--- lernerlabdb/tests/interface_modules_tests/test_Scientist.py
+++ lernerlabdb/tests/interface_modules_tests/test_Scientist.py
@@ -10,10 +10,20 @@ class TestScientist:
         assert scientist.email == 'test'
         assert scientist.net_id == 'test'
         assert not scientist.projects
-        
+
     def test_add_project(self, scientist, project1, project2):
-        scientist.add_project(project1, project2)
-        assert scientist.projects 
+        scientist.add_project([project1, project2])
+        assert scientist.projects == [project1, project2]
         assert scientist.projects[0] == project1
         assert scientist.projects[1] == project2
-
+        
+    def test_data_property(self, scientist, project1):
+        scientist.add_project(project1)
+        data = scientist.data
+        assert isinstance(data, dict)
+        assert data['first_name'] == scientist.first_name
+        assert data['last_name'] == scientist.last_name
+        assert data['email'] == scientist.email
+        assert data['net_id'] == scientist.net_id
+        assert data['projects'] == [project1.data]
+        
