92d91647ff06a628dcaa786a248d87a49199d5a4
mschaid <michael.schaid@northwestern.edu>
2023-11-09 15:14:00 -0600

    Refactor Surgery class tests

--- lernerlabdb/tests/interface_modules_tests/test_Surgery.py
+++ lernerlabdb/tests/interface_modules_tests/test_Surgery.py
@@ -7,35 +7,32 @@ from lernerlabdb.interface_modules.Procedure import Procedure
 from lernerlabdb.interface_modules.Surgery import Surgery
 
 
-@pytest.fixture
-def surgery():
-    return Surgery(1)
 
 
 class TestSurgery:
-    def test_init(self, surgery):
-        assert isinstance(surgery.date, date), "Date property is incorrect."
-        assert isinstance(surgery.time_of_surgery,
+    def test_init(self, surgery1):
+        assert isinstance(surgery1.date, date), "Date property is incorrect."
+        assert isinstance(surgery1.time_of_surgery,
                           time), "Time property is incorrect."
-        assert surgery.surgery_number == 1, "Surgery number property is incorrect."
-        assert isinstance(surgery.procedures,
+        assert surgery1.surgery_number == 1, "Surgery number property is incorrect."
+        assert isinstance(surgery1.procedures,
                           list), "Procedures property is not a list."
-        assert len(surgery.procedures) == 0, "Procedures property is not empty."
-        assert isinstance(surgery.drugs, list), "Drugs property is not a list."
-        assert len(surgery.drugs) == 0, "Drugs property is not empty."
-        assert isinstance(surgery.notes, list), "Notes property is not a list."
-        assert len(surgery.notes) == 0, "Notes property is not empty."
+        assert len(surgery1.procedures) == 0, "Procedures property is not empty."
+        assert isinstance(surgery1.drugs, list), "Drugs property is not a list."
+        assert len(surgery1.drugs) == 0, "Drugs property is not empty."
+        assert isinstance(surgery1.notes, list), "Notes property is not a list."
+        assert len(surgery1.notes) == 0, "Notes property is not empty."
 
-    def test_publics(self, surgery):
-        assert surgery.date_string == datetime.now().strftime(
+    def test_publics(self, surgery1):
+        assert surgery1.date_string == datetime.now().strftime(
             "%m/%d/%Y"), "Date property is incorrect."
-        assert surgery.time_of_surgery_string == datetime.now().strftime(
+        assert surgery1.time_of_surgery_string == datetime.now().strftime(
             "%H:%M"), "Time property is incorrect."
 
-    def test_surgery_data(self, surgery):
-        assert surgery.data == {
-            "Date": surgery.date,
-            "Time of surgery": surgery.time_of_surgery,
+    def test_surgery_data(self, surgery1):
+        assert surgery1.data == {
+            "Date": surgery1.date,
+            "Time of surgery": surgery1.time_of_surgery,
             "Surgery number": 1,
             "Procedures": [],
             "Drugs": [],
