560eaccd11f0b295cd6500231ee293ab8a6735ab
mschaid <michael.schaid@northwestern.edu>
2023-11-10 14:19:03 -0600

    Add docstrings and update Coordinates class constructor

--- lernerlabdb/lernerlabdb/interface_modules/Coordinates.py
+++ lernerlabdb/lernerlabdb/interface_modules/Coordinates.py
@@ -2,18 +2,46 @@ from typing import Dict
 
 
 class Coordinates:
-    ap: float
-    ml: float
-    dv: float
+    """
+    a class to represent coordinates of a brain structure
+    
+    Parameters
+    ----------
+    ap : float default 0.
+        Anteroposterior coordinate
+    ml : float
+        Mediolateral coordinate
+    dv : float
+        Dorsoventral coordinate
+        
+    Attributes
+    ----------
+    ap : float
+        Anteroposterior coordinate
+    ml : float
+        Mediolateral coordinate
+    dv : float
+        Dorsoventral coordinate
+    coordinates : Dict[str, float]
+        A dictionary containing the all coordinates of the structure
+    
+    Examples
+    --------
+    >>> coordinates = Coordinates(1, 2, 3)
+    >>> coordinates.coordinates
+    {'AP': 1, 'ML': 2, 'DV': 3}
 
-    def __init__(self, ap=0, ml=0, dv=0.):
-        self.ap: float = ap
-        self.ml: float = ml
-        self.dv: float = dv
+    """
+
+    def __init__(self, ap=0., ml=0., dv=0.):
+        self.ap = ap
+        self.ml = ml
+        self.dv = dv
 
     @property
     def coordinates(self) -> Dict[str, float]:
-        # if self.ap is None or self.ml is None or self.dv is None:
-        #     return "Coordinates not logged. Please log coordinates."
+        """
+        returns a dictionary of all coordinates
+        """
         coordinates = {"AP": self.ap, "ML": self.ml, "DV": self.dv}
         return coordinates
