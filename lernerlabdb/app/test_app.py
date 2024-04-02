9a1a07e8015bddcffaacb20b5538658bbd1f9bcd
mschaid <michael.schaid@northwestern.edu>
2024-04-02 15:46:07 -0700

    fix test app struct feature

--- lernerlabdb/app/test_app.py
+++ lernerlabdb/app/test_app.py
@@ -1,29 +1,30 @@
 import numpy as np
 from shiny import App, Inputs, Outputs, Session, reactive, req, ui, render
 from lernerlabdb.input_modules import StructureInput, NoteInput, ProcedureInput
-import pretty_errors
 
 
-class AppUI:
+class AppUI():
     @property
     def app_ui(self):
         app_ui_page = ui.page_fluid(
             ui.input_select("numb_structures", "Number of structures in procedure",
                             choices=[i for i in range(1, 11, 1)]
                             ),
-            ui.br(),
-            ui.output_ui('structure')
+            ui.output_ui('procedure_structures_columns')
 
         )
         return app_ui_page
 
 
-def server(input, output, session):
+def server(input: Inputs, output: Outputs, session: Session):
     @render.ui
-    def structure():
-        numb_structs = int(input.numb_structures())
-        procedure_input = ProcedureInput(numb_structs)
-        return procedure_input.procedure_input()
+    def procedure_structures_columns():
+        number_of_structure_inputs = int(input.numb_structures())
+        columns = [StructureInput().structure_input_form(numb)
+                   for numb in range(number_of_structure_inputs)
+                   ]
+        input_columns = ui.TagList(ui.layout_columns(*columns))
+        return input_columns
 
 
 app_ui = AppUI().app_ui
