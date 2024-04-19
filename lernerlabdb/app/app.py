49f84e2549c9c2811a3c7ca4d3b38ca447d648d5
mschaid <michael.schaid@northwestern.edu>
2024-04-19 13:44:09 -0700

    test deploy

--- lernerlabdb/app/app.py
+++ lernerlabdb/app/app.py
@@ -1,45 +1,48 @@
-import pretty_errors
-from lernerlabdb.interface_modules.enums import *
-from typing import List
+# import pretty_errors
+# from lernerlabdb.interface_modules.enums import *
+# from typing import List
 
-from lernerlabdb.input_modules import *
+# from lernerlabdb.input_modules import *
 from shiny import *
 
 # import shinyswatch
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 
-# create sidebar
-# sidebar = sidebar()
+# # create sidebar
+# # sidebar = sidebar()
 
-# create input panels
-mouse_input = MouseInput()
-surgery_input = SurgeryInput()
-procedure_input = ProcedureInput()
-notes_input = NoteInput()
+# # create input panels
+# mouse_input = MouseInput()
+# surgery_input = SurgeryInput()
+# procedure_input = ProcedureInput()
+# notes_input = NoteInput()
 
-# create navigation panels
-mouse_panel = create_mouse_panel(mouse_input)
-surgery_panel = create_surgery_panel(surgery_input)
-procedure_panel = create_procedure_panel(procedure_input)
+# # create navigation panels
+# mouse_panel = create_mouse_panel(mouse_input)
+# surgery_panel = create_surgery_panel(surgery_input)
+# procedure_panel = create_procedure_panel(procedure_input)
 
-# create navigation cards
-navigation_cards = create_navigation_cards(
-    mouse_panel=mouse_panel,
-    surgery_panel=surgery_panel,
-    procedure_panel=procedure_panel
-)
+# # create navigation cards
+# navigation_cards = create_navigation_cards(
+#     mouse_panel=mouse_panel,
+#     surgery_panel=surgery_panel,
+#     procedure_panel=procedure_panel
+# )
 
 # create user
 
 
 app_ui = ui.page_fluid(
+
     # sidebar,
     # shinyswatch.theme_picker_ui(),
     # shinyswatch.theme.minty(),
-    ui.markdown("# Lerner Lab Database"),
-    ui.markdown("---"),
-    navigation_cards,
-    notes_input.display_note_input()
+    ui.markdown("# Mikes Test Deployment"),
+    ui.input_action_button('click_me', "Click me!"),
+    ui.output_ui('say_thanks')
+    # ui.markdown("---"),
+    # navigation_cards,
+    # notes_input.display_note_input()
 )
 
 # default at 0/1 use reactive event to update ui.input
@@ -47,17 +50,20 @@ app_ui = ui.page_fluid(
 
 
 def server(input: Inputs, output: Outputs, session: Session):
-    # shinyswatch.theme_picker_server()
 
     @render.ui
+    @reactive.event(input.click_me)
+    def say_thanks():
+        return ui.markdown("# Thanks!!")
+
+    # shinyswatch.theme_picker_server()
+    # @render.ui
     # @reactive.event(input.add)
-    def procedure_structures_columns():
-        number_of_structure_inputs = int(input.numb_structures())
-        columns = [StructureInput().structure_input_form(numb)
-                   for numb in range(number_of_structure_inputs)
-                   ]
-        input_columns = ui.TagList(ui.layout_columns(*columns))
-        return input_columns
-
-
+    # def procedure_structures_columns():
+    #     number_of_structure_inputs = int(input.numb_structures())
+    #     columns = [StructureInput().structure_input_form(numb)
+    #                for numb in range(number_of_structure_inputs)
+    #                ]
+    #     input_columns = ui.TagList(ui.layout_columns(*columns))
+    #     return input_columns
 app = App(app_ui, server)
